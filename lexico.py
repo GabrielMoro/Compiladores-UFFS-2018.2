fb0a372f90af77146bcd4252aa81ec8f1c80b799
GabrielMoro <gabrielhmoro@gmail.com>
2018-12-02 20:17:35 -0200

    um monte de lixo

--- lexico.py
+++ lexico.py
@@ -1,12 +1,40 @@
-from beautifultable import BeautifulTable
+# from beautifultable import BeautifulTable
 
 AFD = open('AFDparaLex', 'r')
 AFDparaLex = AFD.readlines()
 cdg = open('codigo.txt', 'r')
 codigo = []
 codigo = cdg.readlines()
-sep = [' ', '=', '>', '<', '+', '-', ',', '*', '**', '\'', '/', '\n']
+sep = [' ', '=', '>', '<', '+', '-', ',', '*', '**', '/', '(', ')', ';', '\n']
 afd = {}
+tags = {
+36 : 'int',
+13 : 'var',
+9 : 'equal',
+14 : 'num',
+10 : 'endline',
+33: 'for',
+20: 'endfor',
+57: 'while',
+25: 'endwhile',
+37: 'if',
+52: 'then',
+30: 'else',
+27: 'endif',
+48: 'show',
+44: 'read',
+1: 'comma',
+2: 'slash',
+3: 'asterisk',
+4: 'minus',
+5: 'plus',
+40: 'not',
+6: 'less',
+7: 'more',
+8: 'diff',
+11: 'openpar',
+12: 'closepar',
+}
 
 tabSimb = open('TabelaSimbolos', 'w')
 
@@ -29,7 +57,7 @@ for linhaCodigo in range(len(codigo)):
     fim = 0
     for caractere in range(len(codigo[linhaCodigo])):
         if codigo[linhaCodigo][caractere] not in alfabeto and codigo[linhaCodigo][caractere] not in sep:
-            print("kkkkkk troxa esse caractere nao existe/nao faz parte do alfabeto: " + codigo[linhaCodigo][caractere])
+            print("Esse símbolo não faz parte do alfabeto: " + codigo[linhaCodigo][caractere])
             exit()
         if codigo[linhaCodigo][caractere] not in sep:
             fim += 1
@@ -46,8 +74,8 @@ for linhaCodigo in range(len(codigo)):
         for caractere in range(len(palavra)):
             estadoAtual = int(afd[palavra[caractere]][estadoAtual])
         if estadoAtual == -1:
-            print("Esse token nao existe nessa linguagem, seu ignobil " + palavra)
+            print("Esse token nao existe nessa linguagem: " + palavra)
             exit()
         else:
-            tabSimb.write(palavra+'!'+str(estadoAtual)+'!'+str(linhaAtual)+'\n')
+            tabSimb.write(palavra+'!'+tags[estadoAtual]+'!'+str(linhaAtual)+'\n') #str(estadoAtual)
     linhaAtual += 1
