0e704d98f9186a16ae992b87ddd5d725071d345a
GabrielMoro <gabrielhmoro@gmail.com>
2018-12-05 00:35:56 -0200

    Teoricamente o semântico tá pronto

--- sintatico.py
+++ sintatico.py
@@ -16,6 +16,7 @@ simb_tab.close()
 fita = []
 pilha = []
 regras = []
+ts = []
 acao = ' '
 
 pilha.insert(0, 0)
@@ -23,6 +24,7 @@ pilha.insert(0, 0)
 for simbolos in tabsim:
     simbolo = simbolos.split("!")
     simbolo[2] = simbolo[2][:-1]
+    ts.append(simbolo)
     fita.append(simbolo)
 fita.append([0,'$','$'])
 
@@ -55,3 +57,25 @@ while(acao != 'acc'):
         pilha.insert(0, regras[rgr][0]);
         pilha.insert(0, int(slr['table'][pilha[1]][pilha[0]]));
 print("\nCódigo sintaticamente correto")
+
+declaradas = []
+v = []
+opr = ['/', '*', '-', '+', '=']
+
+for i in range(len(ts)):
+    if(ts[i][1] == 'int'):
+        variaveis = []
+        variaveis.append(ts[i][0])
+    if(ts[i][1] == 'var' and 'int' in variaveis):
+        if(ts[i - 1][0] not in opr):
+            variaveis.append(ts[i][0])
+    if(ts[i][1] == 'endline' and 'int' in variaveis):
+        variaveis.remove('int')
+        if(variaveis):
+            for var in variaveis:
+                if(var in declaradas):
+                    print("\nErro semântico:\n Linha " + ts[i][2] + ": varíavel " + var + " já declarada")
+                    exit(0)
+                declaradas.append(var)
+
+print("\nNão foi detectada nenhuma variavel duplicada")
